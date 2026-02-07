26de4e33f6a2ce111938c9a70b6ef1f511a1370d
M-kalebu <kalebulot@gmail.com>
2026-02-07 18:20:39 +0530

    output topic is added

---
+++ output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "10+10";
+    // cout << "this operator is called insertation" "\n" << "hello world" << endl;
+
+    cout << "this operator is called insertation" << endl
+         << "hello world";
+
+    return 0;
+}
