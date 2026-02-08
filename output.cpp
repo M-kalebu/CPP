adad54ca735a5783fca24ef7afcf2d5d37775015
M-kalebu <kalebulot@gmail.com>
2026-02-08 22:10:23 +0530

    fix

--- output.cpp
+++ output.cpp
@@ -5,6 +5,7 @@ int main()
 {
     // cout << "10+10";
     // cout << "this operator is called insertation" "\n" << "hello world" << endl;
+    // cout << "this operator is called insertation" "\n" << "hello world" << endl;
 
     cout << "this operator is called insertation" << endl
          << "hello world";
