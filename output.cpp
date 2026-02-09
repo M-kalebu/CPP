2e41d2e3012b1541855976735979f033ad0033ad
M-kalebu <kalebulot@gmail.com>
2026-02-09 14:19:17 +0530

    fix it

--- output.cpp
+++ output.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     // cout << "10+10";
+    // cout << "10+10";
     // cout << "this operator is called insertation" "\n" << "hello world" << endl;
     // cout << "this operator is called insertation" "\n" << "hello world" << endl;
 
