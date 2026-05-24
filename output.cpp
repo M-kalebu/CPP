12ef038006c98e673702c5a88a5814937d9cd741
M-kalebu <kalebulot@gmail.com>
2026-05-24 09:17:57 +0530

    24052026Problems

---
+++ output.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << 10 + 10;
+
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string s = "\" Kalebu_0805 \"";
+
+    cout << s << endl;
+
+    cout << 0705 << "im from \n rajahmundry" << endl;
+    cout << "Hello \n developer!";
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+    cout << 0705 << "im from \n rajahmundry" << endl;
+    cout << "Hello \n developer!";
