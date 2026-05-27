b2af26b2ac9a76e9c762718cf8beda6f88caaa2b
M-kalebu <kalebulot@gmail.com>
2026-05-27 22:08:40 +0530

    resolve

--- Problems/P0008.cpp
+++ Problems/P0008.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Welcome to Kalebu's calculator" << endl;
+    cout << "Welcome to kalebu's calculator" << endl;
     cout << "a. Addition" << endl;
     cout << "b. Subtraction" << endl;
     cout << "c. Multiplication" << endl;
     cout << "d. Divison" << endl;
 
-    cout << "choose your option" << endl;
+    cout << "Choose your option" << endl;
     char option;
     cin >> option;
 
@@ -17,7 +17,7 @@ int main()
     int num1, num2;
     cin >> num1 >> num2;
 
-    cout << "Your option is " << option << "  and your two numbers " << num1 << " " << num2 << endl;
+    cout << "Your option is " << option << " and your two numbers " << num1 << " " << num2 << endl;
 
     if (option == 'a')
     {
@@ -25,15 +25,15 @@ int main()
     }
     if (option == 'b')
     {
-        cout << "subtraction is: " << (num1 - num2) << endl;
+        cout << "Subtraction is: " << (num1 - num2) << endl;
     }
     if (option == 'c')
     {
-        cout << "Addition is: " << (num1 * num2) << endl;
+        cout << "Multiplication is: " << (num1 * num2) << endl;
     }
     if (option == 'd')
     {
-        cout << "Addition is: " << (num1 / num2) << endl;
+        cout << "Divison is: " << (num1 / num2) << endl;
     }
 
     return 0;
